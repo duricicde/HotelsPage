b5ceee2dabe2c5dd8eee4903eaa033f66cc57b66
duricic <djuri.cic001@gmail.com>
2023-03-21 13:00:07 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+
+  theme: {
+    extend: {
+      colors: {
+        bgBlue: '#f2f2f2',
+        bgMarin: '#2C4664',
+        borderColor: '#21364E',
+        bgButton: '#C8FF8C',
+        borderGray: '#D8D8D8',
+        borderAbout: '#333333',
+        afterColor: '#192C42',
+        firstGradial: '#7F95AE',
+        secoundGradial: '#E5E9EF',
+        bgIcon: '#3F5E82',
+        bgadvantages: '#EDF1F5'
+
+
+      },
+
+      fontFamily: {
+        sans: ['Nunito Sans']
+      }
+      
+    },
+  },
+  plugins: [],
+};
